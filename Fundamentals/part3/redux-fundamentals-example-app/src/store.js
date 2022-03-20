da0bc1f583bfe96f7828960372093d1d668d3f20
GDS83192 <graham.squires3@gmail.com>
2022-03-20 13:08:46 -0700

    progress

--- Fundamentals/part3/redux-fundamentals-example-app/src/store.js
+++ Fundamentals/part3/redux-fundamentals-example-app/src/store.js
@@ -1,15 +1,22 @@
 import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from './reducer'
-import {sayHiOnDispatch, includeMeaningOfLife} from './exampleAddons/enhancers'
+
 import {print1, print2, print3 } from './exampleAddons/middleware'
 
+
+const composedEnhancer = composeWithDevTools(
+    // Example:and whatever middleware you actually want to use here
+    applyMiddleware(print1, print2, print3)
+)
+
+const store = createStore(rootReducer, composedEnhancer)
 const middlewareEnhancer = applyMiddleware(print1, print2, print3)
 
 // Pass enhancer As the second Arg, since there's no preloaded state
 // const store = createStore(rootReducer)
 
 
-const store = createStore(rootReducer, middlewareEnhancer)
 
 
 
